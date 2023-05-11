71b7cb62d0c1793e5aff9b3e7ae83d9207eac98b
NileshW1812 <wankhadenilesh09@gmail.com>
2023-05-12 00:34:55 +0530

    Chenged in Listner class

--- src/main/java/com/nilesh/project/Listner.java
+++ src/main/java/com/nilesh/project/Listner.java
@@ -1,56 +1,46 @@
 package com.nilesh.project;
 
-
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
-
-public class Listner implements ITestListener{
+public class Listner implements ITestListener {
 
 	public void onTestStart(ITestResult result) {
-		Reporter.log("Test case started "+result.getName());
-		
+		Reporter.log("Test case started " + result.getName());
+
 	}
 
 	public void onTestSuccess(ITestResult result) {
-		Reporter.log("Test case succeded"+result.getName());
-		
+		Reporter.log("Test case succeded" + result.getName());
+
 	}
 
 	public void onTestFailure(ITestResult result) {
-	  Reporter.log("Test case failed "+result.getName());	
-		
+		Reporter.log("Test case failed " + result.getName());
+
 	}
+
 	public void onTestSkipped(ITestResult result) {
-		Reporter.log("Test case skiped "+result.getName());
-		
+		Reporter.log("Test case skiped " + result.getName());
+
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		
-		
+
 	}
 
 	public void onTestFailedWithTimeout(ITestResult result) {
-		
-		
+
 	}
+
 	public void onStart(ITestContext context) {
-		
-		
+
 	}
 
 	public void onFinish(ITestContext context) {
-		
-		
-	}
-	public void github()
-	{
-		
-	}
-	
+
 	}
 
-		
+}
